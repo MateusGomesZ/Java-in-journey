923bb37a8c2bb2493740f87fa3be52f2a6842dda
Mateus Gomes <mateus_888@hotmail.com>
2023-03-12 22:49:39 -0300

    Classe filha Analista, que herda atributos e métodos da classe Funcionario

---
+++ src/Analista.java
@@ -0,0 +1,27 @@
+
+public class Analista extends Funcionario {
+    private String tecnologia;
+
+    public Analista(String nome, double salario, String tecnologia) {
+        super(nome, salario, "Analista");
+        this.tecnologia = tecnologia;
+    }
+
+    public String getTecnologia() {
+        return tecnologia;
+    }
+
+    public void setTecnologia(String tecnologia) {
+        this.tecnologia = tecnologia;
+    }
+
+    // Implementação do método abstrato calcularBonus()
+    public double calcularBonus() {
+        return getSalario() * 0.1;
+    }
+
+    // Sobrescrita do método toString() para exibir informações de um Analista
+    public String toString() {
+        return super.toString() + ", Tecnologia: " + tecnologia;
+    }
+}
